fadb465c01f796ff76f5968c59fa517021ba63fb
Muqadsa Qudoos <muqadsaqudoos@gmail.com>
2023-09-29 06:23:42 -0700

    day 23

---
+++ day_23.py
@@ -0,0 +1,39 @@
+import sys
+
+class Node:
+    def __init__(self,data):
+        self.right=self.left=None
+        self.data = data
+class Solution:
+    def insert(self,root,data):
+        if root==None:
+            return Node(data)
+        else:
+            if data<=root.data:
+                cur=self.insert(root.left,data)
+                root.left=cur
+            else:
+                cur=self.insert(root.right,data)
+                root.right=cur
+        return root
+
+    def levelOrder(self,root):
+        if root is None:
+            return
+        q = []
+        q.append(root)
+        while len(q)!=0:
+            p = q.pop(0)
+            print(p.data, end=' ')
+            if p.left is not None:
+                q.append(p.left)
+            if p.right is not None:
+                q.append(p.right)
+
+T=int(input())
+myTree=Solution()
+root=None
+for i in range(T):
+    data=int(input())
+    root=myTree.insert(root,data)
+myTree.levelOrder(root)
